536dfec8151d2fbd4e5e49280c2fa8dd52c9e453
kev <31861128+piyoki@users.noreply.github.com>
2026-02-21 15:06:42 -0500

    patch: update fish configs

--- .config/fish/config.d/abbr_common.fish
+++ .config/fish/config.d/abbr_common.fish
@@ -7,10 +7,9 @@ abbr - "cd -"
 # system
 abbr ls "ls -ltrh"
 abbr du "du -d 1 -h"
-abbr rm "trash -v"
+abbr trash "trash -v"
 abbr journal "journalctl -xefu"
 
 # pastebin
 alias pb="curl -F 'c=@-' https://fars.ee/"
 alias snips="ssh snips.sh"
-
